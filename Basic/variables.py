ab75504da662bbe67da40af7e1b8ee2b05960ea5
Nur A Alam <nuraalam376@gmail.com>
2021-07-28 21:54:52 +0600

    initial commit

---
+++ Basic/variables.py
@@ -0,0 +1,26 @@
+x = 5
+print(x)
+
+x = "hello World"
+print(x)
+
+x = "123"
+print(type(x))
+
+x = int(x)
+print(type(x))
+
+y = True
+print(y)
+
+
+str = '''
+    This 
+    is 
+    multi line
+'''
+
+print(str)
+
+print(10 + 10)
+print("10" + "10")
